fca21e76c9e45c1ec6a93965b8867197b0885a8b
priestine <priestine1.dev@gmail.com>
2020-05-05 02:00:00 +0300

    :construction: Add GetVersionCommit

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,7 @@ import { execWith } from "./execWith.mjs"
 import { GetLatestVersion } from "./pipes/GetLatestVersion.mjs"
 import { MakeNewVersion } from "./pipes/MakeNewVersion.mjs"
 import { ExitIfNoVersion } from "./pipes/ExitIfNoVersion.mjs"
+import { GetVersionCommit } from "./pipes/GetVersionCommit.mjs"
 import { Log } from "./pipes/Log.mjs"
 
 const argv = process.argv.slice(2)
@@ -14,9 +15,14 @@ const conventions = {
 }
 
 const execOrElse = execWith(execSync)
+const execOrExit = execOrElse(e => {
+  console.error(e.message || e)
+  process.exit(1)
+})
 
 GetLatestVersion(execOrElse(() => "0.0.0"))
   .concat(MakeNewVersion)
   .concat(ExitIfNoVersion(() => process.exit(0)))
+  .concat(GetVersionCommit(execOrExit))
   .concat(Log(console.log))
   .run({ argv })
